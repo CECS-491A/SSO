39669970fb6ffe86dbdc3910da58e2b1564e485b
Julian Poyourow <julianpoyo@gmail.com>
2019-03-10 18:35:21 -0700

    Move logic to manager layer

--- Backend/WebAPI/Controllers/UsersController.cs
+++ Backend/WebAPI/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity.Validation;
 using System.Linq;
 using System.Net;
@@ -8,84 +9,102 @@ using System.Web.Http;
 using System.Web.Http.Cors;
 using DataAccessLayer.Database;
 using DataAccessLayer.Models;
-using ServiceLayer.Services;
+using ManagerLayer;
+using ServiceLayer.Exceptions;
 
 namespace WebAPI.Controllers
 {
+    public class UserRegistrationRequest
+    {
+        [Required]
+        public string email { get; set; }
+        [Required]
+        public string password { get; set; }
+        [Required]
+        public DateTime dob { get; set; }
+        [Required]
+        public string city { get; set; }
+        [Required]
+        public string state { get; set; }
+        [Required]
+        public string country { get; set; }
+        [Required]
+        public string securityQ1 { get; set; }
+        [Required]
+        public string securityQ1Answer { get; set; }
+        [Required]
+        public string securityQ2 { get; set; }
+        [Required]
+        public string securityQ2Answer { get; set; }
+        [Required]
+        public string securityQ3 { get; set; }
+        [Required]
+        public string securityQ3Answer { get; set; }
+    }
+
     [EnableCors(origins: "http://localhost:8081", headers: "*", methods: "*")]
     public class UsersController : ApiController
     {
-        public class RegisterRequest
-        {
-            public string email;
-            public string password;
-
-            public DateTime dob;
-            public string city;
-            public string state;
-            public string country;
-
-            public string securityQ1;
-            public string securityQ1Answer;
-            public string securityQ2;
-            public string securityQ2Answer;
-            public string securityQ3;
-            public string securityQ3Answer;
-        }
-
         [HttpPost]
         [Route("api/users/register")]
-        public IHttpActionResult Register([FromBody] RegisterRequest request)
+        public IHttpActionResult Register([FromBody]UserRegistrationRequest request)
         {
-            try
+            if (!ModelState.IsValid)
             {
-                var valid = new System.Net.Mail.MailAddress(request.email);
+                return BadRequest(ModelState);
             }
-            catch (Exception)
-            {
-                return BadRequest("Email is not valid");
-            }
-            IPasswordService _passwordService = new PasswordService();
-            ISessionService _sessionService = new SessionService();
-
-            byte[] salt = _passwordService.GenerateSalt();
-            string hash = _passwordService.HashPassword(request.password, salt);
-            User user = new User
-            {
-                Email = request.email,
-                PasswordHash = hash,
-                PasswordSalt = salt,
-
-                DateOfBirth = request.dob,
-                City = request.city,
-                State = request.state,
-                Country = request.country,
-
-                SecurityQ1 = request.securityQ1,
-                SecurityQ1Answer = request.securityQ1Answer,
-                SecurityQ2 = request.securityQ2,
-                SecurityQ2Answer = request.securityQ2Answer,
-                SecurityQ3 = request.securityQ3,
-                SecurityQ3Answer = request.securityQ3Answer,
-                UpdatedAt = DateTime.UtcNow,
-                CreatedAt = DateTime.UtcNow
-            };
 
             using (var _db = new DatabaseContext())
             {
-                IUserService _userService = new UserService();
-                var dbUser = _userService.CreateUser(_db, user);
+                User user;
+                try
+                {
+                    UserManager userManager = new UserManager();
+                    user = userManager.CreateUser(
+                        _db,
+                        request.email,
+                        request.password,
+                        request.dob,
+                        request.city,
+                        request.state,
+                        request.country,
+                        request.securityQ1,
+                        request.securityQ1Answer,
+                        request.securityQ2,
+                        request.securityQ2Answer,
+                        request.securityQ3,
+                        request.securityQ3Answer);
+                } catch (ArgumentException ex)
+                {
+                    return Conflict();
+                } catch (FormatException ex)
+                {
+                    return BadRequest("Invalid email address.");
+                } catch (PasswordPwnedException ex)
+                {
+                    return Unauthorized();
+                }
+
+                AuthorizationManager authorizationManager = new AuthorizationManager();
+                Session session = authorizationManager.CreateSession(_db, user);
 
                 try
                 {
                     _db.SaveChanges();
-                    return Ok(new { data = new { token = "faketoken" } });
-                }
-                catch (DbEntityValidationException ex)
+                } catch (DbEntityValidationException ex)
                 {
-                    _db.Entry(dbUser).State = System.Data.Entity.EntityState.Detached;
+                    _db.Entry(user).State = System.Data.Entity.EntityState.Detached;
+                    _db.Entry(session).State = System.Data.Entity.EntityState.Detached;
                     return InternalServerError();
                 }
+
+                return Ok(new
+                {
+                    data = new
+                    {
+                        token = session.Token
+                    }
+                });
             }
         }
     }
